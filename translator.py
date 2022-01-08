4efef202e085179aab4eae9557b9506ef05b485c
Benedict Orlich <benedict.orlich@gmail.com>
2022-01-08 21:55:31 +0000

    Sync with local version

--- translator.py
+++ translator.py
@@ -7,6 +7,17 @@ pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 img = cv2.imread('Screenshot.png')
 # Convert to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+# Detect text from image
+text = pytesseract.image_to_string(img)
+print(text)
+
+# Return each detected character and their bounding boxes
+boxes = pytesseract.image_to_boxes(img)
+print(boxes)
+
+
 # Show the output
 cv2.imshow("Output", img)
 cv2.waitKey(0)
