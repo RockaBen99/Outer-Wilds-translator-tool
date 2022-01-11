1b894230cc8abcb6f8781e937875e8fa74c2d3c3
Benedict Orlich <benedict.orlich@gmail.com>
2022-01-11 18:40:17 +0000

    Rename Screenshot.png to TestScreenshot.png

--- translator.py
+++ translator.py
@@ -6,7 +6,7 @@ import deepl
 pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 
 # Reading image
-img = cv2.imread('Screenshot.png')
+img = cv2.imread('TestScreenshot.png')
 # Convert to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -18,7 +18,6 @@ print(text)
 # Return each detected character and their bounding boxes
 #boxes = pytesseract.image_to_boxes(img)
 #print(boxes)
-
 
 # Show the output
 #cv2.imshow("Output", img)
