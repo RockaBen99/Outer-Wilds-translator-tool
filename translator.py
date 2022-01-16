7de5d84ee58e33057a11c8ad8d2d50bb3504f88a
Benedict Orlich <benedict.orlich@gmail.com>
2022-01-16 17:07:45 +0000

    Add os module and fix translator

--- translator.py
+++ translator.py
@@ -2,11 +2,12 @@ import cv2
 import pytesseract
 #from translate import Translator
 import deepl
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
 
 # Reading image
-img = cv2.imread('TestScreenshot.png')
+img = cv2.imread('photo.jpg')
 # Convert to RGB
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -32,5 +33,5 @@ print(text)
 
 # Translate text
 translator = deepl.Translator("b9ff2aae-ca1f-e192-56ae-a8c7faa94924:fx")
-result = translator.translate_text(text, target_lang="EN")
-print(result)
+result = translator.translate_text(text, target_lang="EN-GB")
+print("\n\n\n\n\n\n\n\n"+str(result))
