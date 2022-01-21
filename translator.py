79e021db02d1c217b8b0f5823d26462e15e6ec5e
Benedict Orlich <benedict.orlich@gmail.com>
2022-01-21 00:02:09 +0000

    add imutils rotate to text detection

--- translator.py
+++ translator.py
@@ -7,6 +7,7 @@ import cv2
 import pytesseract
 import deepl
 import distro
+import imutils
 
 from displayhatmini import DisplayHATMini
 
@@ -41,9 +42,11 @@ def translateImage(image_path):
 # Convert to RGB
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    img = imutils.rotate_bound(img, angle=-90)
+
 # Detect text from image
     text = pytesseract.image_to_string(img)
-    print(text + '\n\n---------------\n\n')
+    print(text + '---------------')
 # Translate text
     translator = deepl.Translator("b9ff2aae-ca1f-e192-56ae-a8c7faa94924:fx")
     result = translator.translate_text(text, target_lang="EN-GB")
