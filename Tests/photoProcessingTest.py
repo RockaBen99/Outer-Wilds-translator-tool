13ca893ff55f9bc655a390adbdca0109d3c011d9
Benedict Orlich <benedict.orlich@gmail.com>
2022-04-13 20:43:56 +0100

    Contrast test modifications

--- Tests/photoProcessingTest.py
+++ Tests/photoProcessingTest.py
@@ -1,5 +1,7 @@
 import cv2
 import imutils
+import pytesseract
+import deepl
 
 img = cv2.imread("photo.jpg")
 #cv2.imwrite("test.jpg", img)
@@ -8,7 +10,7 @@ rotated = imutils.rotate(img,90)
 cv2.imwrite("rotated.jpg", rotated)
 print("done")
 
-def translateImage(image_path):
+def processImage(image_path):
 # Read image
     img = cv2.imread(image_path)
 # Convert to RGB
@@ -18,4 +20,28 @@ def translateImage(image_path):
 
     cv2.imwrite("test.jpg", img)
 
-translateImage("photo.jpg")
+processImage("photo.jpg")
+
+# Tell pytesseract where tesseract is
+pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
+
+def translateImage(image_path):
+# Read image
+    img = cv2.imread(image_path)
+# Convert to RGB
+
+    img = imutils.rotate_bound(img, angle=-90)
+    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Detect text from image
+    text = pytesseract.image_to_string(img)
+    print(text + '---------------')
+# Translate text
+    translator = deepl.Translator("b9ff2aae-ca1f-e192-56ae-a8c7faa94924:fx")
+    result = translator.translate_text(text, target_lang="EN-GB")
+    print(result)
+    mode = "showTranslated"
+    return result, mode
+
+translateImage('photo.jpg')
